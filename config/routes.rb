83709a77357a8390755c5543c42cd4cd08084485
Dave <unsurpassed@yahoo.com>
2013-04-04 21:08:00 -0400

    Add Devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Costumesanity::Application.routes.draw do
+  devise_for :users
+
+  # get "static_pages/home"
+  root :to => 'static_pages#home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
